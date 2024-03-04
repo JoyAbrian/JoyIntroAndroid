e551005301351399164c067d7dda178a12acf430
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-04 16:20:45 +0800

    contact button function

--- app/src/main/java/com/example/joyriotdesignandroid/ContactActivity.java
+++ app/src/main/java/com/example/joyriotdesignandroid/ContactActivity.java
@@ -3,6 +3,7 @@ package com.example.joyriotdesignandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +16,37 @@ public class ContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact);
 
         buttonBack = (Button) findViewById(R.id.buttonPrevious);
-
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(ContactActivity.this, MainActivity.class));
+            }
+        });
+
+        buttonInstagram = (Button) findViewById(R.id.buttonInstagram);
+        buttonInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/joyrantepasang/"));
+                startActivity(intent);
+            }
+        });
+
+        buttonFacebook = (Button) findViewById(R.id.buttonFacebook);
+        buttonFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://web.facebook.com/WHATSUPDUCK33/"));
+                startActivity(intent);
+            }
+        });
+
+        buttonGithub = (Button) findViewById(R.id.buttonGithub);
+        buttonGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/JoyAbrian"));
+                startActivity(intent);
             }
         });
     }

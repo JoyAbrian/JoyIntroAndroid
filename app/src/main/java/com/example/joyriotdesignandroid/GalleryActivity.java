3294a971e1684f7a35054124947b67b451ed506d
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-04 15:43:07 +0800

    button function

--- app/src/main/java/com/example/joyriotdesignandroid/GalleryActivity.java
+++ app/src/main/java/com/example/joyriotdesignandroid/GalleryActivity.java
@@ -2,13 +2,26 @@ package com.example.joyriotdesignandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GalleryActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        button = (Button) findViewById(R.id.buttonNext);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GalleryActivity.this, ContactActivity.class));
+            }
+        });
     }
 }

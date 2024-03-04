0916224df2f4fa05896e279dcd3ee61d4b0169d2
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-04 15:34:47 +0800

    hobbies gaming

--- app/src/main/java/com/example/joyriotdesignandroid/HobbiesActivity.java
+++ app/src/main/java/com/example/joyriotdesignandroid/HobbiesActivity.java
@@ -2,13 +2,27 @@ package com.example.joyriotdesignandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HobbiesActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hobbies);
+
+        button = (Button) findViewById(R.id.buttonNext);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HobbiesActivity.this, GalleryActivity.class));
+            }
+        });
+
     }
 }

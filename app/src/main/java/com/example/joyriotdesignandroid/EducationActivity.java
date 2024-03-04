98b6e1a3bd8166be6837b4d05cbc0522494e7c3e
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-04 15:12:19 +0800

    button function

--- app/src/main/java/com/example/joyriotdesignandroid/EducationActivity.java
+++ app/src/main/java/com/example/joyriotdesignandroid/EducationActivity.java
@@ -2,13 +2,25 @@ package com.example.joyriotdesignandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EducationActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_education);
+
+        button = (Button) findViewById(R.id.buttonNext);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EducationActivity.this, HobbiesActivity.class));
+            }
+        });
     }
 }
